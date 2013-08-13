dae3136cb507f07428584237b5e18c95f3bce4ce
lordlinus <lordlinus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-13 15:07:39 +0000

    Fixed some bugs
    git-svn-id: https://plugins.svn.wordpress.org/personal-chat-room/tags/1.1@755661 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ chat/index.php
@@ -0,0 +1,105 @@
+<?php
+
+// set error reporting level
+if (version_compare(phpversion(), '5.3.0', '>=') == 1)
+  error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+else
+  error_reporting(E_ALL & ~E_NOTICE);
+
+require_once('classes/Services_JSON.php');
+require_once('classes/CMySQL.php'); // including service class to work with database
+require_once('classes/CLogin.php'); // including service class to work with login processing
+
+$sErrors = '';
+if(isset($_POST['value']))
+{
+		
+		$val_data = $_POST['value'];
+		$val_data_unser = unserialize($val_data);
+		$_SESSION['host'] = $val_data_unser[1];
+		$_SESSION['user'] = $val_data_unser[2];
+		$_SESSION['pass'] = $val_data_unser[3];
+		$_SESSION['dbs'] = $val_data_unser[0];
+		$GLOBALS['MySQL']->sDbHost = $_SESSION['host'];
+        $GLOBALS['MySQL']->sDbName = $_SESSION['dbs'];
+        $GLOBALS['MySQL']->sDbUser = $_SESSION['user'];
+        $GLOBALS['MySQL']->sDbPass = $_SESSION['pass'];
+}
+$GLOBALS['MySQL']->sDbHost = $_SESSION['host'];
+$GLOBALS['MySQL']->sDbName = $_SESSION['dbs'];
+$GLOBALS['MySQL']->sDbUser = $_SESSION['user'];
+$GLOBALS['MySQL']->sDbPass = $_SESSION['pass'];
+$GLOBALS['MySQL']->vLink = mysql_connect($GLOBALS['MySQL']->sDbHost, $GLOBALS['MySQL']->sDbUser, $GLOBALS['MySQL']->sDbPass);
+ mysql_select_db($GLOBALS['MySQL']->sDbName, $GLOBALS['MySQL']->vLink);
+// join processing
+if (! isset($_SESSION['member_id']) && $_POST['Join'] == 'Join') {
+    $sUsername = $GLOBALS['MySQL']->escape($_POST['username']);
+    $sFirstname = $GLOBALS['MySQL']->escape($_POST['firstname']);
+    $sLastname = $GLOBALS['MySQL']->escape($_POST['lastname']);
+    $sEmail = $GLOBALS['MySQL']->escape($_POST['email']);
+    $sPassword = $GLOBALS['MySQL']->escape($_POST['password']);
+
+    if ($sUsername && $sEmail && $sPassword) {
+        // check if already exist
+        $aProfile = $GLOBALS['MySQL']->getRow("SELECT * FROM `cs_profiles` WHERE `email`='{$sEmail}'");
+        if ($aProfile['id'] > 0) {
+            $sErrors = '<h2>Another profile with same email already exist</h2>';
+        } else {
+            // generate Salt and Cached password
+            $sSalt = 'testing'; // TODO - we will add generation of salt in future
+            $sPass = sha1(md5($sPassword) . $sSalt);
+
+            // add new member into database
+            $sSQL = "
+                INSERT INTO `cs_profiles` SET 
+                `name` = '{$sUsername}',
+                `first_name` = '{$sFirstname}',
+                `last_name` = '{$sLastname}',
+                `email` = '{$sEmail}',
+                `password` = '{$sPass}',
+                `salt` = '{$sSalt}',
+                `status` = 'active',
+                `role` = '1',
+                `date_reg` = NOW();
+            ";
+            $GLOBALS['MySQL']->res($sSQL);
+
+            // autologin
+            $GLOBALS['CLogin']->performLogin($sUsername, $sPassword);
+        }
+    }
+}
+
+// login system init and generation code
+$sLoginForm = $GLOBALS['CLogin']->getLoginBox();
+
+$sChat = '<h2>You do not have rights to use chat</h2>';
+
+$sInput = '';
+if ($_SESSION['member_id'] && $_SESSION['member_status'] == 'active' && $_SESSION['member_role']) {
+    require_once('classes/CChat.php'); // including service class to work with chat
+
+    // get last messages
+    $sChat = $GLOBALS['MainChat']->getMessages();
+    if ($_GET['action'] == 'get_last_messages') { // regular updating of messages in chat
+        $oJson = new Services_JSON();
+        header('Content-type: application/json');
+        echo $oJson->encode(array('messages' => $sChat));
+        exit;
+    }
+
+    // get input form
+    $sInput = $GLOBALS['MainChat']->getInputForm();
+
+    if ($_POST['message']) { // POST-ing of message
+        $iRes = $GLOBALS['MainChat']->acceptMessage();
+
+        $oJson = new Services_JSON();
+        header('Content-type: application/json');
+        echo $oJson->encode(array('result' => $iRes));
+        exit;
+    }
+}
+
+// draw common page
+echo strtr(file_get_contents('templates/main_page.html'), array('{form}' => $sLoginForm . $sErrors, '{chat}' => $sChat, '{input}' => $sInput));
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Plugin Name ===
+Contributors: lordlinus
+Tags: chat with online members, online chat, chat, personal chat room, chat room
+Requires at least: 2.0.2
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Personal Chat Room adds a facility to allow your customers to chat with one another. Here They can questions with another users and can get the answers
+from another users of your website.
+
+== Description ==
+
+Personal Chat Room adds a facility to allow your customers to chat with one another. Here They can questions with another users and can get the answers
+from another users of your website.
+
+For any bug or report or error or suggetions or feedback, you can post the support
+
+Basically It has the following features : 
+ * Add a sidebar button to your site to allow your customers to chat
+ * Add shortcode PERSONAL_CHAT_ROOM to any page or post to activate the plugin
+ * Private Chat (Premium Feature)
+
+
+== Installation ==
+
+Here is what you need to do: 
+
+1. Install plugin OR unzip the downloaded file to /wp-content/plugins/
+1. Go to your plugin settings and enable the plugin.
+3. Go to "Chat Room" link and save the configuration
+3. save your database configuration using dashboard
+4. Add `PERSONAL_CHAT_ROOM` shortcode to your header, post, page or widget
+
+== Screenshots ==
+
+1. Wordpress Admin Setting
+2. Wordpress Front Page Display
+3. After clicking on the Live chat button, you will be redirected to this page
+
+
+
+== Changelog ==
+
+= 1.0 =
+* First Version
+
+= 1.1 =
+* Remove some bugs
